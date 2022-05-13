1d53ac73f8c12f5918d5fff09312341c6f885508
rahulbala2909 <52848280+rahulbala2909@users.noreply.github.com>
2022-05-13 15:37:51 +0530

    Crud using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Pricecard from './components/Pricecard';
+function Price(props){
+
+ let data = [
+    {
+      product:"FREE",
+      price:0,
+      users:"Single User",
+      userEnabler:true,
+      storage:"5GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"Community Access",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:false,
+      phoneSupport:"Dedicated Phone Support",
+      phoneSupportEnabler:false,
+      subDomain:"Free Subdomain",
+      subDomainEnabler:false,
+      reports:"Monthly Status Report",
+      reportsEnabler:false
+    },
+
+    {
+      product:"PRO",
+      price:49,
+      users:"5 User",
+      userEnabler:true,
+      storage:"50GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"Community Access",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      phoneSupport:"Dedicated Phone Support",
+      phoneSupportEnabler:true,
+      subDomain:"Free Subdomain",
+      subDomainEnabler:true,
+      reports:"Monthly Status Report",
+      reportsEnabler:false
+    },
+
+    {
+      product:"PREMIUIM",
+      price:99,
+      users:"Unlimited Users",
+      userEnabler:true,
+      storage:"150GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"Community Access",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      phoneSupport:"Dedicated Phone Support",
+      phoneSupportEnabler:true,
+      subDomain:"Unlimited Free Subdomains",
+      subDomainEnabler:true,
+      reports:"Monthly Status Report",
+      reportsEnabler:true
+    }
+  ] 
+
+
+
+
+
+  return <>
+    <section className='pricing py-5'>
+      <div className='constainer'>
+        <div className='row'>
+          {
+            data.map((e)=>{
+              return <Pricecard data={e}/>
+            })
+          }
+        </div>
+      </div>
+    </section>
+</>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Price/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
